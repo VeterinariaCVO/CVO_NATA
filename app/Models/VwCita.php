e38dca2488782f44b4303097676eb15d5f20beee
Carlos Alberto Garcia Padilla <carlos.garcia@GOSSTORREON.local>
2026-04-13 13:16:59 -0600

    .

--- app/Models/VwCita.php
+++ app/Models/VwCita.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VwCita extends Model
+{
+    protected $table = 'vw_citas';
+    public $timestamps = false;
+    protected $primaryKey = 'cita_id';
+
+    protected static function boot() {
+        parent::boot();
+        static::saving(fn() => false);
+    }
+}
